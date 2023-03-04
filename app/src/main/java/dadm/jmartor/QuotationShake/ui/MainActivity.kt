57c715820e61fa4f59409676d3b91d5bc248110e
tr3sar <josepcdg@gmail.com>
2023-03-04 18:52:34 +0100

    Execisi 1

--- app/src/main/java/dadm/jmartor/QuotationShake/ui/MainActivity.kt
+++ app/src/main/java/dadm/jmartor/QuotationShake/ui/MainActivity.kt
@@ -1,11 +1,14 @@
-package dadm.jmartor.QuotationShake
+package dadm.jmartor.QuotationShake.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dadm.jmartor.QuotationShake.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
