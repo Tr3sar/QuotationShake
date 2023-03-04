74d868081710f64a02e1bf65fa3b4425254098e2
tr3sar <josepcdg@gmail.com>
2023-03-04 20:03:46 +0100

    Exercisi 3

--- app/src/main/java/dadm/jmartor/QuotationShake/ui/MainActivity.kt
+++ app/src/main/java/dadm/jmartor/QuotationShake/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package dadm.jmartor.QuotationShake.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import dadm.jmartor.QuotationShake.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,10 @@ class MainActivity : AppCompatActivity() {
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //Estava diferent en el pdf de la pràctica
+        val navController = binding.fragmentContainerView.getFragment<NavHostFragment>().navController
+
+        binding.bnvInicial.setupWithNavController(navController)
     }
 }
