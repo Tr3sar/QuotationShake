7246b69b6d9a2cab09350d70763501b300619be6
tr3sar <josepcdg@gmail.com>
2023-03-04 18:46:08 +0100

    Initial commit

---
+++ app/src/main/java/dadm/jmartor/QuotationShake/MainActivity.kt
@@ -0,0 +1,11 @@
+package dadm.jmartor.QuotationShake
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
