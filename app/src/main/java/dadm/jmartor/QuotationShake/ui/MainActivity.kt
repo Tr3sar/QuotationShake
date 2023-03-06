33ef3b1c189908202e449a4fa80c7f378ac45047
tr3sar <josepcdg@gmail.com>
2023-03-06 18:48:30 +0100

    Exercisi 6

--- app/src/main/java/dadm/jmartor/QuotationShake/ui/MainActivity.kt
+++ app/src/main/java/dadm/jmartor/QuotationShake/ui/MainActivity.kt
@@ -2,11 +2,17 @@ package dadm.jmartor.QuotationShake.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.core.view.MenuProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationBarView
+import dadm.jmartor.QuotationShake.R
 import dadm.jmartor.QuotationShake.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,5 +28,22 @@ class MainActivity : AppCompatActivity() {
         (binding.bnvInicial as NavigationBarView).setupWithNavController(navController)
         setSupportActionBar(binding.materialToolbar5)
         setupActionBarWithNavController(navController, AppBarConfiguration(navController.graph))
+
+        addMenuProvider(object: MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_about, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when(menuItem.itemId) {
+                    R.id.aboutDialogFragment -> {
+                        navController.navigate(R.id.aboutDialogFragment)
+                        Log.i("Dentro del when", "si")
+                        true
+                    }
+                    else -> false
+                }
+            }
+        })
     }
 }
