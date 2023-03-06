54d88b283b337281fb8884840192f0be221bca66
tr3sar <josepcdg@gmail.com>
2023-03-06 20:20:21 +0100

    Exercisi 7 + correcció del 6

--- app/src/main/java/dadm/jmartor/QuotationShake/ui/MainActivity.kt
+++ app/src/main/java/dadm/jmartor/QuotationShake/ui/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.core.view.MenuProvider
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -15,7 +16,10 @@ import com.google.android.material.navigation.NavigationBarView
 import dadm.jmartor.QuotationShake.R
 import dadm.jmartor.QuotationShake.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MenuProvider {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,27 +27,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         //Estava diferent en el pdf de la pràctica
-        val navController = binding.fragmentContainerView.getFragment<NavHostFragment>().navController
+        navController = binding.fragmentContainerView.getFragment<NavHostFragment>().navController
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.newSettingsFragment, R.id.newQuotationFragment, R.id.newFavouritesFragment))
 
         (binding.bnvInicial as NavigationBarView).setupWithNavController(navController)
         setSupportActionBar(binding.materialToolbar5)
-        setupActionBarWithNavController(navController, AppBarConfiguration(navController.graph))
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
-        addMenuProvider(object: MenuProvider {
-            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-                menuInflater.inflate(R.menu.menu_about, menu)
-            }
+        addMenuProvider(this)
+    }
 
-            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-                return when(menuItem.itemId) {
-                    R.id.aboutDialogFragment -> {
-                        navController.navigate(R.id.aboutDialogFragment)
-                        Log.i("Dentro del when", "si")
-                        true
-                    }
-                    else -> false
-                }
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_about, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when(menuItem.itemId) {
+            R.id.aboutDialogFragment -> {
+                navController.navigate(R.id.aboutDialogFragment)
+                true
             }
-        })
+            else -> false
+        }
     }
 }
