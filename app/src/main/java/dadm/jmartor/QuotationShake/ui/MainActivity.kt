1e5633b6bcb7781dd403c02ad8fb40ab67e49c3a
tr3sar <josepcdg@gmail.com>
2023-03-06 17:19:24 +0100

    Exercisi 5

--- app/src/main/java/dadm/jmartor/QuotationShake/ui/MainActivity.kt
+++ app/src/main/java/dadm/jmartor/QuotationShake/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationBarView
 import dadm.jmartor.QuotationShake.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,8 +19,7 @@ class MainActivity : AppCompatActivity() {
         //Estava diferent en el pdf de la pràctica
         val navController = binding.fragmentContainerView.getFragment<NavHostFragment>().navController
 
-        binding.bnvInicial.setupWithNavController(navController)
-
+        (binding.bnvInicial as NavigationBarView).setupWithNavController(navController)
         setSupportActionBar(binding.materialToolbar5)
         setupActionBarWithNavController(navController, AppBarConfiguration(navController.graph))
     }
