f1667a04fa6c5d844fe2c006ff6138d78950b112
tr3sar <josepcdg@gmail.com>
2023-03-26 11:58:25 +0200

    Prac 3 Exercisi 1

--- app/src/main/java/dadm/jmartor/QuotationShake/ui/settings/NewSettingsFragment.kt
+++ app/src/main/java/dadm/jmartor/QuotationShake/ui/settings/NewSettingsFragment.kt
@@ -3,21 +3,12 @@ package dadm.jmartor.QuotationShake.ui.settings
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
 import dadm.jmartor.QuotationShake.R
 import dadm.jmartor.QuotationShake.databinding.FragmentSettingsBinding
 
-class NewSettingsFragment: Fragment(R.layout.fragment_settings) {
-    private var _binding: FragmentSettingsBinding? = null;
-    private val binding get() = _binding!!;
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        _binding = FragmentSettingsBinding.bind(view);
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null;
+class NewSettingsFragment: PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences_settings, rootKey)
     }
 }
